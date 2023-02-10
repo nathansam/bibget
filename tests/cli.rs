3697102d76f3bd61a86163485d8fc02f5510e0b9
Nathan Constantine-Cooke <nathan.constantine-cooke@ed.ac.uk>
2023-02-10 11:00:45 +0000

    Lint

--- tests/cli.rs
+++ tests/cli.rs
@@ -6,8 +6,8 @@ use std::process::Command; // Run programs
 fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("bibget")?;
     cmd.arg("10.1002/sim.1186");
-    cmd.assert()
-        .success()
-        .stdout(predicate::str::contains("title = {Quantifying heterogeneity in a meta-analysis},"));
+    cmd.assert().success().stdout(predicate::str::contains(
+        "title = {Quantifying heterogeneity in a meta-analysis},",
+    ));
     Ok(())
 }
